6c7397a094abd829818ab6234894dd526ee3ac1b
jaozc <50885531+jaozc@users.noreply.github.com>
2022-10-09 17:07:37 -0300

    Add homepage and cart files
    Adicionados os arquivos referentes a homepage e ao carrinho  de compras

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function simulaComprar(){
+  alert("Produto adicionado ao seu carrinho!");
+}
+
+function finalizaCompra(){
+  alert("Sua compra foi finalizada!");
+  window.location.replace("../pages/homepage.html");
+}
