2c655b4c986a211eea3781b7cad05544299a5443
Leonardo Perassoli <leoperassoli@hotmail.com>
2022-10-09 23:21:19 -0300

    Adicionando página do produto

--- js/index.js
+++ js/index.js
@@ -6,3 +6,7 @@ function finalizaCompra(){
   alert("Sua compra foi finalizada!");
   window.location.replace("../pages/homepage.html");
 }
+
+function calculaCep(){
+  alert("A entrega é R$15 e o prazo é 5 dias úteis.")
+}
